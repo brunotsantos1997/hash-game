1fbb348e2977587f9cf64391a94463af627bc48c
drmistery <bruno.t.santos1997@hotmail.com>
2021-12-26 23:36:28 -0300

    ✨the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+type Players = "O" | "X";
 
 function App() {
+  const [turn, setTurn] = useState<Players>("O");
+  const [winner, setWinner] = useState<Players | null>(null);
+  const [draw, setDraw] = useState<boolean | null>(null);
+
+  /* Vamos armazenar as posições como um objeto
+  {
+    "0": "X",
+    "1": "O"
+    "2": "X"
+  }
+  */
+  const [marks, setMarks] = useState<{ [key: string]: Players }>({});
+  const gameOver = !!winner || !!draw;
+
+  const getSquares = () => {
+    return new Array(9).fill(true);
+  };
+
+  const play = (index: number) => {
+    if (marks[index] || gameOver) {
+      return;
+    }
+
+    setMarks((prev) => ({ ...prev, [index]: turn }));
+    setTurn((prev) => (prev === "O" ? "X" : "O"));
+  };
+
+  const getCellPlayer = (index: number) => {
+    if (!marks[index]) {
+      return;
+    }
+
+    return marks[index];
+  };
+
+  const getWinner = () => {
+    const victoryLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (const line of victoryLines) {
+      const [a, b, c] = line;
+
+      if (marks[a] && marks[a] === marks[b] && marks[a] === marks[c]) {
+        return marks[a];
+      }
+    }
+  };
+
+  const reset = () => {
+    setTurn(marks[0] === "O" ? "X" : "O");
+    setMarks({});
+    setWinner(null);
+    setDraw(null);
+  };
+
+  useEffect(() => {
+    const winner = getWinner();
+
+    if (winner) {
+      setWinner(winner);
+    } else {
+      if (Object.keys(marks).length === 9) {
+        setDraw(true);
+      }
+    }
+  }, [marks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {winner && <h1>{winner} ganhou</h1>}
+
+      {draw && <h1>Empate</h1>}
+
+      {gameOver && <button onClick={reset}>Jogar novamente</button>}
+
+      {!gameOver && <p>É a vez de {turn}</p>}
+
+      <div className={`board ${gameOver ? "gameOver" : null}`}>
+        {getSquares().map((_, i) => (
+          <div className={`cell ${getCellPlayer(i)}`} onClick={() => play(i)}>
+            {marks[i]}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
